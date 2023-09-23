dfda8e0944b5210c6e97e85613141a15c3b610ef
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2023-09-23 00:15:23 -0400

    organize

--- PocketSafeNative/App.tsx
+++ PocketSafeNative/App.tsx
@@ -7,45 +7,6 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Pressable, StyleSheet} from 'react-native';
 import WorldcoinLogo from './components/WorldcoinLogo';
 
-const styles = StyleSheet.create({
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 10,
-    paddingRight: 20,
-    borderRadius: 12,
-    elevation: 3,
-    backgroundColor: 'black',
-    flexDirection: 'row',
-    gap: 10,
-  },
-  text: {
-    fontSize: 16,
-    lineHeight: 21,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-    color: 'white',
-  },
-});
-
-function HomeScreen() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-      <Pressable style={styles.button}>
-        <WorldcoinLogo />
-
-        <Text style={styles.text}>Sign in with Worldcoin</Text>
-      </Pressable>
-    </View>
-  );
-}
-
 const Stack = createNativeStackNavigator();
 
 function App() {
---
+++ PocketSafeNative/screens/HomeScreen.js
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Pressable, StyleSheet} from 'react-native';
+import WorldcoinLogo from '../components/WorldcoinLogo';
+
+
+function HomeScreen() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Pressable style={styles.button}>
+          <WorldcoinLogo />
+  
+          <Text style={styles.text}>Sign in with Worldcoin</Text>
+        </Pressable>
+      </View>
+    );
+  }
+
+
+  const styles = StyleSheet.create({
+    button: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 12,
+      paddingHorizontal: 10,
+      paddingRight: 20,
+      borderRadius: 12,
+      elevation: 3,
+      backgroundColor: 'black',
+      flexDirection: 'row',
+      gap: 10,
+    },
+    text: {
+      fontSize: 16,
+      lineHeight: 21,
+      fontWeight: 'bold',
+      letterSpacing: 0.25,
+      color: 'white',
+    },
+  });
+
+
+  export default HomeScreen
