da4519257a183550c87d40a6ea10fb6f210a422b
Ori Mizrahi <orizmizrahi@gmail.com>
2023-09-22 21:53:43 -0400

    add margin top to transak

--- pocket-safe/src/App.tsx
+++ pocket-safe/src/App.tsx
@@ -15,6 +15,7 @@ function App() {
           width: "100vw",
           height: "100vh",
           border: "none",
+          marginTop: "25px",
         }}
         src="https://global-stg.transak.com?apiKey=c71ecd4a-0819-46a7-8d63-c8b7148aaf63&amp;environment=STAGING&amp;sdkVersion=1.3.0&amp;widgetHeight=625px&amp;widgetWidth=500px"
       />
