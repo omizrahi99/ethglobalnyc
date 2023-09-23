8350b68e28d6e42ecff74268c7f2d685a34a0782
Ori Mizrahi <orizmizrahi@gmail.com>
2023-09-22 21:45:52 -0400

    use ngrok url

--- PocketSafeNative/App.tsx
+++ PocketSafeNative/App.tsx
@@ -66,7 +66,7 @@ function App(): JSX.Element {
 
   return (
     <WebView
-      source={{uri: 'http://localhost:5174'}}
+      source={{uri: 'https://a475-208-123-173-93.ngrok-free.app/'}}
       injectedJavaScript={`const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `}
       scalesPageToFit={false}
     />
