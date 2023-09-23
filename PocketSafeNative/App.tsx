4f65baf6cb7990c4995e640d623872b3386449fc
shaunakk <shaunak@ucsb.edu>
2023-09-23 11:39:45 -0400

    fix

--- PocketSafeNative/App.tsx
+++ PocketSafeNative/App.tsx
@@ -1,6 +1,3 @@
-
-import { Enclave } from './helpers'
-=======
 // In App.js in a new project
 
 import * as React from 'react';
@@ -13,68 +10,16 @@ import {
   DarkTheme,
 } from '@react-navigation/native';
 import {Auth0Provider} from 'react-native-auth0';
+import {Enclave} from './helpers'
+const Stack = createNativeStackNavigator();
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import { WebView } from 'react-native-webview';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-// function Section({children, title}: SectionProps): JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// }
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-  useEffect(() => {
+function App() {
+  React.useEffect(() => {
     const getPair = async () => {
       console.log(await Enclave.generateKeyPair())
     }
     getPair()
   }, [])
-  return (
-    <WebView
-      source={{ uri: 'http://localhost:5174' }}
-      injectedJavaScript={`const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `}
-      scalesPageToFit={false}
-    />
-const Stack = createNativeStackNavigator();
-
-function App() {
   const scheme = useColorScheme();
   return (
     <Auth0Provider
